8ae4f7edfd1dc59bfbf411362f68dffd57acf61d
Basketballforce <28318597+Basketballforce@users.noreply.github.com>
2022-01-16 09:48:21 -0500

    The js works

--- toggleMode.js
+++ toggleMode.js
@@ -1,38 +1,49 @@
+// Mode toggling built with significant help from https://css-tricks.com/author/mohamedadhuham/ 
+// on https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
+
 // Select the icon
 const sunMoon = document.querySelector("#sunMoon")
+
 // set alternative icon styles
 const sun = ' <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#999" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>'
 const moon =' <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#999" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>'
 
+// select the current css theme
+const cssSheet = document.querySelector("#theme-link")
 
 // check for default presence of alternative/secondary mode (dark mode)
 // Check for dark mode preference on the browser/os preference
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)")
 
 // Get the user's theme preference from local storage, if there
-const currentTheme = localStorage.getItem("theme");
+const currentTheme = localStorage.getItem("theme")
 
 if (currentTheme == "dark") {
-  sunMoon.innerHTML = moon
-  document.body.classList.toggle("dark-mode");
-
-} else if (currentTheme == "light") {
   sunMoon.innerHTML = sun
-  document.body.classList.toggle("light-mode");
+  cssSheet.href = "./darkDolphin.css"
+  document.body.classList.toggle("dark-mode")
+
+} else {
+  sunMoon.innerHTML = moon
+  cssSheet.href = "./lightDolphin.css"
+  document.body.classList.toggle("light-mode")
 }
+
+console.log(currentTheme)
+console.log(sunMoon.innerHTML)
 
 // Listen for a click on the icon 
 sunMoon.addEventListener("click", function() {
   // If the user's OS setting is dark and matches our .dark-mode class...
   if (prefersDarkScheme.matches) {
     // ...then toggle the light mode class
-    sunMoon.innerHTML = sun
-    document.body.classList.toggle("light-mode");
+    cssSheet.href = "./lightDolphin.css"
+    document.body.classList.toggle("light-mode")
     // ...but use .dark-mode if the .light-mode class is already on the body,
     var theme = document.body.classList.contains("light-mode") ? "light" : "dark";
   } else {
     // Otherwise, let's do the same thing, but for .dark-mode
-    sunMoon.innerHTML = moon
+    cssSheet.href = "./darkDolphin.css";
     document.body.classList.toggle("dark-mode");
     var theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
   }
@@ -40,12 +51,14 @@ sunMoon.addEventListener("click", function() {
   localStorage.setItem("theme", theme);
 
   if(theme=="dark"){
-    sunMoon.innerHTML = moon
-  }else{
     sunMoon.innerHTML = sun
+    cssSheet.href = "./darkDolphin.css";
+  }else{
+    sunMoon.innerHTML = moon
+    cssSheet.href = "./lightDolphin.css"
   }
   console.log(theme)
-});
+})
 
 
 
